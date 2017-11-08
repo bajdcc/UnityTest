9f92cf62a1ba7b3f200e6dd049d4a83f69be7060
bajdcc <bajdcc@foxmail.com>
2017-11-08 13:48:24 +0800

    Initial commit

---
+++ Assets/createCubes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createCubes : MonoBehaviour {
+
+	private float m_Padding = 0.4f;
+	private float m_Size = 1f;
+
+	// Use this for initialization
+	void Start () {
+		var arr = new int[,]{
+			{ 5, 6, 1, 4, 3, 2 },
+			{ 4, 1, 3, 2, 6, 5 },
+			{ 2, 3, 6, 1, 5, 4 },
+			{ 6, 5, 4, 3, 2, 1 },
+			{ 1, 2, 5, 6, 4, 3 },
+			{ 3, 4, 2, 5, 1, 6 }
+		};
+
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				var cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				cube.transform.localScale = new Vector3 (1, arr[i,j], 1);
+				cube.transform.position = new Vector3 (i * (m_Size + m_Padding), arr[i,j]/2.0f, j * (m_Size + m_Padding));
+				var material = new Material (Shader.Find("Unlit/Color"));
+				material.color = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+				cube.GetComponent<MeshRenderer> ().material = material;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/init.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	//方向灵敏度
+	public float sensitivityKeyboardX = 0.1F;
+	public float sensitivityKeyboardY = 0.1F;
+
+	//方向灵敏度
+	public float sensitivityX = 10F;
+	public float sensitivityY = 10F;
+
+	//上下最大视角(Y视角)
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+
+	private Vector3 PreMouseMPos;
+	private Vector3 PreMouseLPos;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+
+		CameraMove ();
+		MouseScrollWheel ();
+		Move ();
+	}
+
+	private void MouseScrollWheel ()
+	{
+		//滚轮前进后退
+		if (Input.GetAxis ("Mouse ScrollWheel") != 0) {
+			gameObject.transform.Translate (new Vector3 (0, 0, Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * 500));
+		}
+	}
+
+	private void CameraMove ()
+	{
+		//鼠标中键按下，上下左右移动相机
+		if (Input.GetMouseButton (2)) {
+			if (PreMouseMPos.x <= 0) {
+				PreMouseMPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+			} else {
+				Vector3 CurMouseMPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+				Vector3 offset = CurMouseMPos - PreMouseMPos;
+				offset = -offset * 0.1f;//0.1这个数字的大小可以调节速度
+				transform.Translate (offset);
+				PreMouseMPos = CurMouseMPos;
+			}
+		} else {
+			if (Input.GetAxis("Horizontal") != 0)
+			{
+				transform.Translate(Input.GetAxis("Horizontal") * sensitivityKeyboardX, 0, 0);
+			}
+			if (Input.GetAxis("Vertical") != 0)
+			{
+				transform.Translate(0, Input.GetAxis("Vertical") * sensitivityKeyboardY, 0);
+			}
+			PreMouseMPos = new Vector3 (0.0f, 0.0f, 0.0f);
+		}
+	}
+
+	private float m_TurnSpeed = 8f;                    // How fast the rig will rotate from user input.
+	private float m_LookAngleX = 0f;                   // The rig's x axis rotation.
+	private float m_LookAngleY = 0f;                   // The rig's y axis rotation.
+
+	private void Move ()
+	{
+		if(Time.timeScale < float.Epsilon)
+			return;
+
+		if (!Input.GetMouseButton (1))
+			return;
+		
+		// Read the user input
+		var x = Input.GetAxis("Mouse X");
+		var y = Input.GetAxis("Mouse Y");
+
+		// Adjust the look angle by an amount proportional to the turn speed and horizontal input.
+		m_LookAngleX += y*m_TurnSpeed;
+		m_LookAngleY += x*m_TurnSpeed;
+
+		// Rotate the rig (the root object) around Y axis only:
+		transform.localRotation = Quaternion.Euler(-m_LookAngleX, m_LookAngleY, 0f);
+	}
+}
